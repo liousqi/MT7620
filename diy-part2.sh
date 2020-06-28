0df3d6078704014edbe3087d185d965254602940
rayqu918 <66419131+rayqu918@users.noreply.github.com>
2020-06-28 20:13:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,11 +10,3 @@
 
 # Modify default IP
 #sed -i 's/192.168.19.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-#添加软件包 
-git clone https://github.com/rayqu918/openwrt-packages/tree/master/luci-app-passwall
-git clone https://github.com/rayqu918/openwrt-packages/tree/master/luci-app-ssr-plus
-
-#更新
-./scripts/feeds update -a
-./scripts/feeds install -a
